94851d7d49b17853e02b7bcacbf26c97f41edd0a
Shaswat Rungta <shaswatrungta@hotmail.com>
2017-10-07 15:31:06 +0530

    Added meetings manager

---
+++ attend-management/Controllers/MeetingController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+
+namespace attend_management.Controllers
+{
+    [RoutePrefix("api/v1.0/meetings")]
+    public class MeetingController : ApiController
+    {
+        // GET api/v1.0/meetings/{id}
+        [Route("{id}")]
+        [SwaggerOperation("GetById")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public string Get(Guid id)
+        {
+            return "value";
+        }
+    }
+}
